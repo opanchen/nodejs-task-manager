81f01f197fbc15fb37c9b83c5cc7d58c173eda82
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-23 16:57:25 +0100

    upd server starting with db connection

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
 
 // middleware
-
 app.use(express.json());
 
 // routes
@@ -16,4 +17,16 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      port,
+      console.log(`DB is connected. \nServer is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log("Connection starting error: ", error);
+  }
+};
+
+start();
